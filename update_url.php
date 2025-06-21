61535b67c66872d4d054362baa31cf24e399f681
masterplan769 <planscaut2@gmail.com>
2025-06-21 04:52:33 +0200

    Update update_url.php
    Signed-off-by: masterplan769 <planscaut2@gmail.com>

--- update_url.php
+++ update_url.php
@@ -1,7 +1,7 @@
 <?php
 // --- Konfiguráció ---
 // GitHub repó helyi mappája (pl. a te gépeden)
-$repoPath = "$repoPath = "C:/Users/Planscaut/masterplan769.github.io/mypage";";
+$repoPath = "C:/Users/Planscaut/masterplan769.github.io/mypage";
 
 // Frissítendő Cloudflare Tunnel URL
 $currentUrl = "https://your-current-tunnel-url.trycloudflare.com";
@@ -15,16 +15,22 @@ $jsonFile = $repoPath . "/current-url.json";
 $jsonData = json_encode(["url" => $currentUrl], JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES);
 
 // JSON fájl írása
-file_put_contents($jsonFile, $jsonData);
+if (file_put_contents($jsonFile, $jsonData) === false) {
+    die("Nem sikerült írni a JSON fájlt: $jsonFile\n");
+}
 
-// Git műveletek
+// Ellenőrizzük, hogy a repó mappa létezik-e
+if (!is_dir($repoPath)) {
+    die("Nem található a repó mappa: $repoPath\n");
+}
+
+// Átváltás a repó mappára
 chdir($repoPath);
 
 // Git add
 exec("git add current-url.json", $outputAdd, $returnAdd);
 if ($returnAdd !== 0) {
-    echo "Hiba a git add során.\n";
-    exit(1);
+    die("Hiba a git add során.\n");
 }
 
 // Git commit (csak ha van változás)
@@ -32,15 +38,13 @@ exec('git diff --cached --quiet', $outputDiff, $returnDiff);
 if ($returnDiff !== 0) {
     exec('git commit -m "Automatikus tunnel URL frissítés"', $outputCommit, $returnCommit);
     if ($returnCommit !== 0) {
-        echo "Hiba a git commit során.\n";
-        exit(1);
+        die("Hiba a git commit során.\n");
     }
 
     // Git push
     exec('git push origin main', $outputPush, $returnPush);
     if ($returnPush !== 0) {
-        echo "Hiba a git push során.\n";
-        exit(1);
+        die("Hiba a git push során.\n");
     }
 
     echo "Tunnel URL frissítve és feltöltve GitHub-ra.\n";
